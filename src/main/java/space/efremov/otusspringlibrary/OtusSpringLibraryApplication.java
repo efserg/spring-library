f34d3b1fa45e1b82c2e60cd4e5a16aaa9795be96
Ефремов Сергей Юрьевич (ЗК-0000155) <sergey.y.efremov@bia-tech.ru>
2018-07-27 10:53:49 +0300

    Initial directory structure

---
+++ src/main/java/space/efremov/otusspringlibrary/OtusSpringLibraryApplication.java
@@ -0,0 +1,12 @@
+package space.efremov.otusspringlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusSpringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusSpringLibraryApplication.class, args);
+	}
+}
