653521b43862dd61a2eb58f3bb84076b7c872463
Sergey Y. Efremov <mynog@yandex.ru>
2018-08-06 14:43:48 +0300

    Update Book CLI (it can create book with exist autors and tags now)

--- src/main/java/space/efremov/otusspringlibrary/controller/impl/console/BookConsoleController.java
+++ src/main/java/space/efremov/otusspringlibrary/controller/impl/console/BookConsoleController.java
@@ -8,14 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 import space.efremov.otusspringlibrary.domain.Author;
 import space.efremov.otusspringlibrary.domain.Book;
 import space.efremov.otusspringlibrary.domain.Publisher;
+import space.efremov.otusspringlibrary.domain.Tag;
 import space.efremov.otusspringlibrary.exception.EntityNotFoundException;
 import space.efremov.otusspringlibrary.repository.AuthorRepository;
 import space.efremov.otusspringlibrary.repository.BookRepository;
 import space.efremov.otusspringlibrary.repository.PublisherRepository;
+import space.efremov.otusspringlibrary.repository.TagRepository;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ShellComponent
 @ShellCommandGroup("book")
@@ -25,11 +28,13 @@ public class BookConsoleController {
     private final BookRepository bookRepository;
     private final PublisherRepository publisherRepository;
     private final AuthorRepository authorRepository;
+    private final TagRepository tagRepository;
 
-    public BookConsoleController(BookRepository bookRepository, PublisherRepository publisherRepository, AuthorRepository authorRepository) {
+    public BookConsoleController(BookRepository bookRepository, PublisherRepository publisherRepository, AuthorRepository authorRepository, TagRepository tagRepository) {
         this.bookRepository = bookRepository;
         this.publisherRepository = publisherRepository;
         this.authorRepository = authorRepository;
+        this.tagRepository = tagRepository;
     }
 
     @Transactional
@@ -37,15 +42,14 @@ public class BookConsoleController {
     public Book add(@ShellOption(help = "Book title. Use quotes if you need, e.g. \"Large Scale Machine Learning with Python\".", value = {"title", "book-title", "bookTitle"}) String title,
                     @ShellOption(help = "Book ISBN.", value = {"isbn", "book-isbn", "bookIsbn"}) String isbn,
                     @ShellOption(help = "Book year.", value = {"year", "book-year", "bookYear"}) Integer year,
-                    @ShellOption(help = "Publisher ID. Use \"publisher list\" command to find publisher ID", value = {"publisher-id", "pid", "publisherId"}) Long publisherId
-//                    @ShellOption(help = "Author IDs list. Use \"author list\" command to find author ID", arity = 2, value = {"authors", "author-list", "author-ids, authorIds"}) Long[] authorIds
+                    @ShellOption(help = "Publisher ID. Use \"publisher list\" command to find publisher ID", value = {"publisher-id", "pid", "publisherId"}) Long publisherId,
+                    @ShellOption(help = "Author IDs list, separated by \",\". Use \"author list\" command to find author ID", defaultValue = "", value = {"authors", "author-list", "author-ids, authorIds"}) String authorIds,
+                    @ShellOption(help = "Tag IDs list, separated by \",\". Use \"tag list\" command to find tag ID", defaultValue = "", value = {"tags", "tag-list", "tag-ids, tagIds"}) String tagIds
     ) {
         final Publisher publisher = publisherRepository.findById(publisherId).orElseThrow(EntityNotFoundException::new);
-//        final List<Author> authors = authorRepository.findAllById(Arrays.asList(authorIds));
-//        if (authorIds.length != authors.size()) {
-//            throw new EntityNotFoundException();
-//        }
-        return bookRepository.save(new Book(title, isbn, year, publisher, Collections.emptyList(), Collections.emptyList()));
+        final List<Author> authors = Arrays.stream(authorIds.split(",")).map(Long::new).map(authorRepository::findById).map(a -> a.orElseThrow(EntityNotFoundException::new)).collect(Collectors.toList());
+        final List<Tag> tags = Arrays.stream(tagIds.split(",")).map(Long::new).map(tagRepository::findById).map(a -> a.orElseThrow(EntityNotFoundException::new)).collect(Collectors.toList());
+        return bookRepository.save(new Book(title, isbn, year, publisher, tags, authors));
     }
 
     @Transactional
